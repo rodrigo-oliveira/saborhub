5720d9bbadd3db09117d8cfff3ad36314c143953
Rodrigo Oliveira <rodrigo_oliveira@live.com>
2025-08-09 16:45:43 -0300

    feat: configuração inicial do projeto SaborHub com Spring Boot

---
+++ src/test/java/com/saborhub/SaborhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saborhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
